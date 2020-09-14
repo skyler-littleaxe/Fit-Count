3f9fc553590433dfd8faab6840431f72716f0224
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-09-13 23:06:11 -0500

    changed last migration file column names(pre-migration) and added association between user and exercise

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,2 +1,6 @@
 class Exercise < ActiveRecord::Base
+    belongs_to :lifter, class_name: "User"
+    validates :exercise_name presence: true
+    validates :exercise_notes presence: true
+
 end
--- db/migrate/20200913185935_create_exercises.rb
+++ db/migrate/20200913185935_create_exercises.rb
@@ -1,8 +1,8 @@
 class CreateExercises < ActiveRecord::Migration
   def change
     create_table :exercises do |t|
-      t.string :title
-      t.text :name
+      t.string :exercise_name
+      t.text :exercise_notes
       t.integer :lifter_id
 
       t.timestamps null: false
