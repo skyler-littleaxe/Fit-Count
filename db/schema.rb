e7fa260183302a4072a138c4ce89a255d86200df
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-09-16 22:37:52 -0500

    fixed dependency error in exercise model, ran migration for exercises table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200910043453) do
+ActiveRecord::Schema.define(version: 20200913185935) do
+
+  create_table "exercises", force: :cascade do |t|
+    t.string   "exercise_name"
+    t.text     "exercise_notes"
+    t.integer  "lifter_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
